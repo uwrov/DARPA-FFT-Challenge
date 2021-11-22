2043b50632f3d006805e8ac6eb920895bfab89e1
Andrew <anemnox@gmail.com>
2021-11-22 03:31:26 -0800

    Sped up weather_mode

--- model/weather_model.py
+++ model/weather_model.py
@@ -2,20 +2,21 @@ import numpy as np
 import pygrib
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+import time
 
 SCALE = 360
 
 class GribVectorField:
     def __init__(self, grb):
         self.grb = grb
-        size = self.grb.values.shape
-        lat, long = grb.latlons()
-        self.min_lat = lat.min()
-        self.max_lat = lat.max()
-        self.min_long = long.min()
-        self.max_long = long.max()
-        self.d_lat = (self.max_lat - self.min_lat) / size[0]
-        self.d_long = (self.max_long - self.min_long) / size[1]
+        self.shape = self.grb.values.shape
+        self.lat, self.long = grb.latlons()
+        self.min_lat = self.lat.min()
+        self.max_lat = self.lat.max()
+        self.min_long = self.long.min()
+        self.max_long = self.long.max()
+        self.d_lat = (self.max_lat - self.min_lat) / self.shape[0]
+        self.d_long = (self.max_long - self.min_long) / self.shape[1]
         self.vals = np.ma.filled(self.grb.values, -SCALE)
         ''' Visualizing data
         data=grb.values
@@ -43,17 +44,15 @@ class GribVectorField:
     def get_field(self, long, lat, size):
         field = list()
         l_list = list()
-        la, lo = self.grb.latlons()
-        shape = self.grb.values.shape
         lat_ind = int(((self.max_lat - lat) // self.d_lat) - size // 2)
         long_ind = int(((long - self.min_long) // self.d_long) - size // 2)
         for i in range(lat_ind, lat_ind + size):
             #lat_list = list()
             #i_list = list()
             for j in range(long_ind, long_ind + size):
-                if i >= 0 and j >= 0 and i < shape[0] and j < shape[1]:
+                if i >= 0 and j >= 0 and i < self.shape[0] and j < self.shape[1]:
                     field.append(self.vals[i, j] / SCALE)
-                    l_list.append((lo[i, j], la[i, j]))
+                    l_list.append((self.long[i, j], self.lat[i, j]))
                 else:
                     field.append(np.ma.masked)
             #field.append(lat_list)
